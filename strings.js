0fc53838ac1d9b3390687ffb08ad0ed7c65929fa
Alexander Garbalev <garbalevalex@mail.ru>
2021-11-16 19:57:28 +0300

    reverseString

--- strings.js
+++ strings.js
@@ -1,23 +1,20 @@
-let string = 'Hello, my name is Alex, i am fine';
+let string = "Hello, my name is Alex, i am fine, i am 123";
 
-//1
+//1 USING RECURSION
 function revStrWithRecursion(str) {
   if (str === "") return "";
   return revStrWithRecursion(str.substr(1)) + str[0];
 }
 console.log(revStrWithRecursion(string));
 
-//2
-function revStrWithArray(str) {
-  // return str.split('').reverse().join('');
-  return [...str].reverse().join('');
-}
+//2 USING ARRAY
+const revStrWithArray = (str) => [...str].reverse().join("");
 
-console.log(revStrWithArray(string))
+console.log(revStrWithArray(string));
 
-//3
+//3 USING ITERATION V.1
 function firstRevStrWithLoop(str) {
-  let newStr = '';
+  let newStr = "";
   for (let i = str.length - 1; i >= 0; i--) {
     newStr += str[i];
   }
@@ -26,31 +23,21 @@ function firstRevStrWithLoop(str) {
 
 console.log(firstRevStrWithLoop(string));
 
-//4
+//4 USING ITERATION V.2
 function secondRevStrWithLoop(str) {
-  let newStr = '';
-  for(let letter of str) {
+  let newStr = "";
+  for (let letter of str) {
     newStr = letter + newStr;
   }
-  return newStr
+  return newStr;
 }
 
 console.log(secondRevStrWithLoop(string));
 
-
-//5 
-
-const funcy = (word) => word.toUpperCase();
-
+//5 REVERSE WORDS, NOT SCTRING
 function revStrPreserveOrder(str) {
-  let newStr = str.replace(/w+/g, 'obama');
-  return newStr
+  let newStr = str.replace(/\w+/gi, (str) => [...str].reverse().join(""));
+  return newStr;
 }
 
 console.log(revStrPreserveOrder(string));
-
-
-let newStr = string.replace(/[A-Z]/gi, 'obama');
-console.log(newStr);
-
-// alert(string.replaceAll('am', 'JJ'));
