77eb438a06e0a3796d85cee9cad29282b642da62
Alexander Garbalev <garbalevalex@mail.ru>
2021-11-16 13:43:02 +0300

    stringReplacer

---
+++ recursion.js
@@ -0,0 +1,19 @@
+/// Recursion
+
+
+function recStr(str){
+  if (str === '') return '';
+  return recStr(str.substr(1)) + str[0];
+}
+console.log(recStr('Sasha')); 
+
+
+function pow(x, n) {
+  if (n < 0) return;
+  if (n == 0) return 1;
+  return x * pow(x, n-1);
+}
+
+
+console.log(pow(2,5));
+
--- strings.js
+++ strings.js
@@ -1,5 +1,56 @@
-function revStr(str) {
+let string = 'Hello, my name is Alex, i am fine';
+
+//1
+function revStrWithRecursion(str) {
   if (str === "") return "";
-  return revStr(str.substr(1)) + str[0];
+  return revStrWithRecursion(str.substr(1)) + str[0];
 }
-console.log(revStr("Sasha"));
+console.log(revStrWithRecursion(string));
+
+//2
+function revStrWithArray(str) {
+  // return str.split('').reverse().join('');
+  return [...str].reverse().join('');
+}
+
+console.log(revStrWithArray(string))
+
+//3
+function firstRevStrWithLoop(str) {
+  let newStr = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr += str[i];
+  }
+  return newStr;
+}
+
+console.log(firstRevStrWithLoop(string));
+
+//4
+function secondRevStrWithLoop(str) {
+  let newStr = '';
+  for(let letter of str) {
+    newStr = letter + newStr;
+  }
+  return newStr
+}
+
+console.log(secondRevStrWithLoop(string));
+
+
+//5 
+
+const funcy = (word) => word.toUpperCase();
+
+function revStrPreserveOrder(str) {
+  let newStr = str.replace(/w+/g, 'obama');
+  return newStr
+}
+
+console.log(revStrPreserveOrder(string));
+
+
+let newStr = string.replace(/[A-Z]/gi, 'obama');
+console.log(newStr);
+
+// alert(string.replaceAll('am', 'JJ'));
