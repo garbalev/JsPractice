5ac38a52aeab9bb91e73e7fa44550a5aded8e674
Alexander Garbalev <garbalevalex@mail.ru>
2021-11-23 15:37:26 +0300

    recursionTasks

--- recursion.js
+++ recursion.js
@@ -1,25 +1,29 @@
 /// Recursion
 
-
-function recStr(str){
-  if (str === '') return '';
+function recStr(str) {
+  if (str === "") return "";
   return recStr(str.substr(1)) + str[0];
 }
-console.log(recStr('Sasha')); 
-
+console.log(recStr("Sasha"));
 
 function pow(x, n) {
   if (n < 0) return;
   if (n == 0) return 1;
-  return x * pow(x, n-1);
+  return x * pow(x, n - 1);
 }
 
+console.log(pow(2, 5));
 
-console.log(pow(2,5));
-
-function fac1(num) {
+function fac(num) {
   if (num === 0) {
     return 1;
   }
-  return num * fac1(num - 1);
+  return num * fac(num - 1);
+}
+
+function repeatStringNumTimes(str, num) {
+  if (num < 1) {
+    return "";
+  }
+  return str + repeatStringNumTimes(str, num - 1);
 }
